699708721f5a2811e1e32023298a68179aacc708
Saurabh Pal <bestpal97@gmail.com>
2025-07-21 14:36:06 +0530

    icon replacement and dark color (#1)
    * minor changes
    
    * cosmetic changes
    
    * Revert "cosmetic changes"
    
    This reverts commit 617238e1a45968a552f496953d67082ac75d0c28.

--- pages/side-panel/src/types/message.ts
+++ pages/side-panel/src/types/message.ts
@@ -2,36 +2,36 @@ export const ACTOR_PROFILES = {
   user: {
     name: 'User',
     icon: 'icons/user.svg',
-    iconBackground: '#4CAF50',
+    iconBackground: '#5d8ed7ff',
   },
   system: {
     name: 'System',
     icon: 'icons/system.svg',
-    iconBackground: '#2196F3',
+    iconBackground: '#e0ac1bff',
   },
   planner: {
     name: 'Planner',
     icon: 'icons/planner.svg',
-    iconBackground: '#FF9800',
+    iconBackground: '#21b156ff',
   },
   navigator: {
     name: 'Navigator',
     icon: 'icons/navigator.svg',
-    iconBackground: '#40A9FF',
+    iconBackground: '#2abac2ff',
   },
   validator: {
     name: 'Validator',
     icon: 'icons/validator.svg',
-    iconBackground: '#EC407A',
+    iconBackground: '#d76b8fff',
   },
   manager: {
     name: 'Manager',
     icon: 'icons/manager.svg',
-    iconBackground: '#9C27B0',
+    iconBackground: '#d17be0ff',
   },
   evaluator: {
     name: 'Evaluator',
     icon: 'icons/evaluator.svg',
-    iconBackground: '#795548',
+    iconBackground: '#e2d95aff',
   },
 } as const;
